b06b0eaa44e7f99dc25fc13dc8d12052fa79f40c
Sumitkb21 <sumitkumar83339@gmail.com>
2024-02-12 16:15:09 +0530

    new version

---
+++ myapp/src/components/home.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import Navbar from './Navbar'
+import './navbar.css'
+import './home.css'
+import myimage from './myphoto.jpg' 
+
+export default function Home() {
+  return (
+    <>
+    <Navbar/>
+
+    {/* const [count,setcount] = usestate(0); */}
+    {/* <button onClick={ ()=>setcount(count+1) }>increment</button> */}
+
+    <section id="intro">
+      <div className="left-content">
+        <span className="hello">Hello, </span>
+        <span className="introtext">
+          I'm <span className="introname">Sumit</span> <br />
+          Software developer <br />
+          {/* & <br />
+          Software developer */}
+        </span>
+        
+        
+      </div>
+
+      <img className="right-content" src={myimage} alt="" />
+    </section>
+    </>
+  )
+}
